cbf428c7c08ebc3f05b4fe6bc96b91ce08ec1186
Obed101 <obedamoako92@gmail.com>
2021-11-13 13:05:44 -0800

    added more commands

--- console.py
+++ console.py
@@ -19,18 +19,54 @@ class HBNBCommand(cmd.Cmd):
         sys.exit(1)
 
     def emptyline(self):
+        """Overrides the builtin `emptyline`"""
         pass
 
     def do_create(self, args):
         """Create command creates a new object"""
-        if args is not None:
-            args = BaseModel()
-            args.save()
-            print(args.id)
-        elif args != "BaseModel":
-            print("** class doesn't exist **")
+        if args[0]:
+            if args == "BaseModel":
+                args = BaseModel()
+                args.save()
+                print(args.id)
+            elif args != "BaseModel":
+                print("** class doesn't exist **")
+                return
         else:
             print("** class name missing **")
+            return
+
+    def do_show(self, args):
+        """displays the Object name and Id"""
+        if args is None:
+            print("** class name missing **")
+        else:
+            args.split()
+            args[0] = BaseModel()
+            my_path = models.engine.file_storage.FileStorage.classes
+            if args[1] not in my_path.keys():
+                print("** class doesn't exist **")
+            else:
+                print(args.__str__())
+
+    def do_destroy(self, args):
+        """deletes the instance, provided name and Id"""
+        dele = args.split()
+        arguments = "{}.{}".format(dele[0], dele[1])
+        if len(dele) == 0:
+            print("** class name missing **")
+            return
+        if len(dele) == 1:
+            print("** instance id missing **")
+            return
+        if dele[0] not in FileStorage.classes.keys():
+            print("** class doesn't exist **")
+            return
+        if arguments not in models.storage.all().keys():
+            print("** no instance found **")
+            return
+        del models.storage.all()[arguments]
+        models.storage.save
 
     do_EOF = do_quit
 
