43cd42c7f2dfbffb0ba2482cc98a96afed6c5f60
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2021-11-13 12:21:45 +0300

    Fix logic when outputing object as dict.

--- models/base_model.py
+++ models/base_model.py
@@ -18,15 +18,15 @@ class BaseModel:
         Returns:
             None
         """
-        self.format = "%Y-%m-%dT%H:%M:%S.%f"
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now().isoformat()
-            self.updated_at = datetime.now().isoformat()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
         else:
             for key in kwargs.keys():
                 if key == 'created_at' or key == 'updated_at':
-                    kwargs[key] = datetime.strptime(kwargs[key], self.format)
+                    kwargs[key] = datetime.strptime(kwargs[key],
+                                                    "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, kwargs[key])
                 elif key != '__class__':
                     setattr(self, key, kwargs[key])
@@ -52,7 +52,7 @@ class BaseModel:
         Returns:
             None
         """
-        self.updated_at = datetime.datetime.now().isoformat()
+        self.updated_at = datetime.now()
         return None
 
     def to_dict(self):
@@ -64,5 +64,10 @@ class BaseModel:
         Returns:
             Dictionary representation of all attributes of object
         """
-        self.__dict__["__class__"] = self.__class__.__name__
-        return self.__dict__
+        object_dict = {}
+        object_dict.update(self.__dict__)
+        object_dict.__setitem__('__class__', self.__class__.__name__)
+        object_dict['created_at'] = self.created_at.isoformat()
+        object_dict['updated_at'] = self.updated_at.isoformat()
+
+        return object_dict
