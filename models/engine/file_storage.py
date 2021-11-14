329edf2f153b28a95634c07d1437ea7d46925187
Obed101 <obedamoako92@gmail.com>
2021-11-14 12:42:13 -0800

    changes for console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,6 +16,23 @@ class FileStorage:
         """Returns the dictionary @__objects"""
         return self.__class__.__objects
 
+    def to_dict(self):
+        """Return a dictionary of attributes using __dict__.
+
+        Args:
+            self (object): Refers to instantiated object.
+
+        Returns:
+            Dictionary representation of all attributes of object
+        """
+        object_dict = {}
+        object_dict.update(self.__dict__)
+        object_dict.__setitem__('__class__', self.__class__.__name__)
+        object_dict['created_at'] = self.created_at.isoformat()
+        object_dict['updated_at'] = self.updated_at.isoformat()
+
+        return object_dict
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         fspath = obj.__class__.__name__
@@ -23,18 +40,19 @@ class FileStorage:
 
     def save(self):
         """serializes __objects to the json file"""
-        with open(FileStorage.__file_path, "r+") as t:
-            json.dump(FileStorage.__objects, t)
+        with open(FileStorage.__file_path, "w") as t:
+            for key, value in FileStorage.__objects.items:
+                json.dump({key: value.to_dict()}, t)
 
     def reload(self):
         """deserializes the json file to __objects"""
         if not path.exists(FileStorage.__file_path):
             return None
-        else:
-            with open(self.__class__.__file_path, "r") as fle:
-                to_dict = json.loads(fle.read())
-            for key in to_dict.keys():
-                for cls in FileStorage.classes.keys():
-                    if cls in key:
-                        FileStorage.__objects[key] = FileStorage.classes[cls\
-                        ](**(to_dict[key]))
+        try:
+            with open(self.__file_path, 'r') as fle:
+                back = json.load(fle)
+                for key in back.values():
+                    cls = key["__class__"]
+                    self.new(eval(cls)(**key))
+        except Exception:
+            pass
