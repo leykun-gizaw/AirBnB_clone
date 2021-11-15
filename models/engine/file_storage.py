353f63a5693ebb7b00fac1c1031f556344454728
Obed101 <obedamoako92@gmail.com>
2021-11-15 12:51:50 -0800

    changed one file

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,34 +4,6 @@ import json
 from os import path
 from models.base_model import BaseModel
 
-
-class FileStorage:
-    """serializes and deserializes an instance"""
-    __file_path = "file.json"
-    __objects = {}
-    classes = {'BaseModel': BaseModel, 'FileStorage': "FileStorage"}
-
-    def all(self):
-        """Returns the dictionary @__objects"""
-        return self.__class__.__objects
-
-    def to_dict(self):
-        """Return a dictionary of attributes using __dict__.
-
-        Args:
-            self (object): Refers to instantiated object.
-
-        Returns:
-            Dictionary representation of all attributes of object
-        """
-        object_dict = {}
-        object_dict.update(self.__dict__)
-        object_dict.__setitem__('__class__', self.__class__.__name__)
-        object_dict['created_at'] = self.created_at.isoformat()
-        object_dict['updated_at'] = self.updated_at.isoformat()
-
-        return object_dict
-
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         fspath = obj.__class__.__name__
